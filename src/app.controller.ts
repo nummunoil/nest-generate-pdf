a68d6eb864e0403b469165390503fd06e9b7b570
Oil <sudarat@1moby.com>
2022-08-11 17:26:08 +0700

    gen pdf

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import * as fs from 'fs';
+import * as PdfPrinter from 'pdfmake';
 
 @Controller()
 export class AppController {
@@ -8,5 +10,49 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('/generatePDF')
+  generatePDF() {
+    const fonts = {
+      Helvetica: {
+        normal: 'Helvetica',
+        bold: 'Helvetica-Bold',
+        italics: 'Helvetica-Oblique',
+        bolditalics: 'Helvetica-BoldOblique',
+      },
+    };
+    const printer = new PdfPrinter(fonts);
+
+    const docDefinition = {
+      content: [
+        { text: 'Heading', fontSize: 25 },
+        {
+          layout: 'lightHorizontalLines', // optional
+          table: {
+            headerRows: 1,
+            widths: ['*', 'auto', 100, '*'],
+
+            body: [
+              ['First', 'Second', 'Third', 'The last one'],
+              ['Value 1', 'Value 2', 'Value 3', 'Value 4'],
+              ['Val 1', 'Val 2', 'Val 3', 'Val 4'],
+            ],
+          },
+        },
+        { text: 'google', link: 'http://google.com', pageBreak: 'before' },
+        { qr: 'text in QR', foreground: 'green', background: 'white' },
+      ],
+      defaultStyle: {
+        font: 'Helvetica',
+      },
+    };
+
+    const options = {};
+    const file_name = 'PDF' + new Date().getTime() + '.pdf';
+    const pdfDoc = printer.createPdfKitDocument(docDefinition, options);
+    pdfDoc.pipe(fs.createWriteStream(file_name));
+    pdfDoc.end();
+    return { file_name: file_name };
   }
 }
